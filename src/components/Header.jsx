2be1131ab8605a6c14245115e20e84a5dc91b5e0
Leonardo Lodi <your_email@address>
2024-08-08 14:08:08 +0100

    Fix: change text from about section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import '../assets/styles/Header.css'
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faBars,
@@ -18,51 +18,45 @@ export default function Header() {
     i18n: { changeLanguage, language },
   } = useTranslation()
 
-  const handleChangeLanguage = () => {
+  const toggleLanguage = useCallback(() => {
     const newLanguage = language === 'en' ? 'pt' : 'en'
     localStorage.setItem('lng', newLanguage)
     changeLanguage(newLanguage)
-  }
+    setAsideVisible(false)
+  }, [language, changeLanguage])
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const menu = document.querySelector('#menu-mobile')
-      const nav = document.querySelector('header nav')
-      const div = document.querySelector('header #header-btns')
+  const handleScroll = useCallback(() => {
+    const menu = document.querySelector('#menu-mobile')
+    const nav = document.querySelector('header nav')
+    const div = document.querySelector('header #header-btns')
 
-      if (window.scrollY > 100) {
-        if (menu !== null) {
-          menu.classList.add('scrolled')
-        }
-
-        nav.classList.add('scrolled')
-        div.classList.add('scrolled')
-      } else {
-        if (menu !== null) {
-          menu.classList.remove('scrolled')
-        }
-
-        nav.classList.remove('scrolled')
-        div.classList.remove('scrolled')
-      }
+    if (window.scrollY > 100) {
+      menu?.classList.add('scrolled')
+      nav.classList.add('scrolled')
+      div.classList.add('scrolled')
+    } else {
+      menu?.classList.remove('scrolled')
+      nav.classList.remove('scrolled')
+      div.classList.remove('scrolled')
     }
-
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
   }, [])
 
-  useEffect(() => {
-    document.addEventListener('mousedown', handleOutsideClick)
-    return () => {
-      document.removeEventListener('mousedown', handleOutsideClick)
-    }
-  })
-
-  const handleOutsideClick = (e) => {
+  const handleEventOutside = useCallback((e) => {
     if (ref.current && !ref.current.contains(e.target)) {
       setAsideVisible(false)
     }
-  }
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    document.addEventListener('mousedown', handleEventOutside)
+    document.addEventListener('scroll', handleEventOutside)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+      document.removeEventListener('mousedown', handleEventOutside)
+      document.removeEventListener('scroll', handleEventOutside)
+    }
+  }, [handleScroll, handleEventOutside])
 
   return (
     <>
@@ -72,9 +66,11 @@ export default function Header() {
             <li>
               <a href="#home">{t('home')}</a>
             </li>
+
             <li>
               <a href="#about">{t('about')}</a>
             </li>
+
             <li>
               <a href="#contact">{t('contact')}</a>
             </li>
@@ -87,7 +83,7 @@ export default function Header() {
             <span>{t('resume')}</span>
           </picture>
 
-          <picture onClick={handleChangeLanguage}>
+          <picture onClick={toggleLanguage}>
             <FontAwesomeIcon icon={faGlobe} className="header-icon" />
             <span>{language === 'en' ? 'En' : 'Pt'}</span>
           </picture>
@@ -127,8 +123,8 @@ export default function Header() {
               <FontAwesomeIcon icon={faFileArrowDown} className="icon" />
               <span>{t('resume')}</span>
             </li>
-
-            <li className="menu" onClick={handleChangeLanguage}>
+            
+            <li className="menu" onClick={toggleLanguage}>
               <FontAwesomeIcon icon={faGlobe} className="icon" />
               <span>{language === 'en' ? 'En' : 'Pt'}</span>
             </li>
